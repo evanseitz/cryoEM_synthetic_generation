e29a0d190038b7f4e1f199f65e0ea179ffce7231
Evan Seitz <35609503+evanseitz@users.noreply.github.com>
2019-11-08 15:00:18 -0500

    Generate first copy of each MRC

---
+++ 4_GenMRC_eman/e2pdb2mrc.sh
@@ -0,0 +1,9 @@
+# script to convert all re-centered PDBs into MRCs (`_1.mrc` represents the 1st copy of each)
+
+for i in ../3_ShiftPDB_phenix/PDB_shifts/*.pdb
+do
+    out=`basename ${i%.*}_1.mrc`
+    outFile=/mnt/Data2/evanseitz/Hsp90_Online/4_GenMRC_eman/MRCs/$out
+    echo "$outFile"
+    e2pdb2mrc.py "${i}" $outFile -A 1 -R 2 -B 250 #pixel size, resolution, box size
+done
