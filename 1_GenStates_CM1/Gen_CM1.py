75ec47c792525d7016e87038a624aca375ee5792
Evan Seitz <35609503+evanseitz@users.noreply.github.com>
2019-11-08 14:49:11 -0500

    Generate first set of conformational motions (CM1)

---
+++ 1_GenStates_CM1/Gen_CM1.py
@@ -0,0 +1,30 @@
+import os
+from chimera import runCommand as rc
+
+# INSTRUCTIONS: run this file from same directory as mono_A_snip.pdb,
+    # and mono_B_snip.pdb are located... CC1 states will output to
+    # Generate_CC1 folder
+
+pyDir = os.path.dirname(os.path.abspath(__file__)) #python file location
+parDir = os.path.abspath(os.path.join(pyDir, os.pardir))
+outDir = os.path.join(pyDir, 'Generate_CC1')
+monoA = os.path.join(pyDir, 'monoA_snip.pdb')
+monoB = os.path.join(pyDir, 'monoB_snip.pdb')
+
+rc('open' + monoB)
+rc('open' + monoA)
+rc('select :677.a') #select central hinge atom (LEU 674)
+
+rot = 1
+for i in range(1,21): #-20 degrees total
+    if i == 1: #don't rotate for state 1
+        rc('combine #0-1, name CC1_%s' % i)
+        rc('write relative #0 format pdb #2 %s/CC1_0%s.pdb' % (outDir,i))
+    else:
+        rc('turn 0,1,0 %d center sel models #0' % rot)
+        rc('combine #0-1, name CC1_%s' % i)
+        if i < 10:
+            rc('write relative #0 format pdb #2 %s/CC1_0%s.pdb' % (outDir,i))
+        else:
+            rc('write relative #0 format pdb #2 %s/CC1_%s.pdb' % (outDir,i))
+    rc('close #2')
