0d9f002a581ebad43d52bbb6b165f7ad06ba2c1d
Evan Seitz <35609503+evanseitz@users.noreply.github.com>
2019-11-08 14:58:07 -0500

    Update shiftPDB_phenix.sh

--- 3_ShiftPDB_phenix/shiftPDB_phenix.sh
+++ 3_ShiftPDB_phenix/shiftPDB_phenix.sh
@@ -1,10 +1,10 @@
 # script to re-center all PDBs based on common center-of-mass coordinates
 
-for i in ../2_GenStates_CC2/Generate_CC2/*.pdb
+for i in ../2_GenStates_CM2/Generate_CM2/*.pdb
 do
     out=`basename $i`
     outFile=/mnt/Data2/evanseitz/Hsp90_Online/3_ShiftPDB_phenix/PDB_shifts/$out
     echo "$outFile"
-    phenix.pdbtools "${i}" output.file_name=$outFile sites.translate="115.5 58.1 -39.5"
+    phenix.pdbtools "${i}" output.file_name=$outFile sites.translate="115.5 58.1 -39.5" #replace with CoM of your state_01_01.pdb
 done
 
