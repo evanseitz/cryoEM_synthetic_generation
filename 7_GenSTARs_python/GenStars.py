7809f3209b5b9e9ca51336793da51e164ec2631f
Evan Seitz <35609503+evanseitz@users.noreply.github.com>
2019-11-22 16:35:47 -0500

    Add files via upload

--- 7_GenSTARs_python/GenStars.py
+++ 7_GenSTARs_python/GenStars.py
@@ -2,6 +2,9 @@ import os, os.path, sys
 import numpy as np
 import mrcfile
 import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import quatsGaussian
 
 mrcPaths = []
 pyDir = os.path.dirname(os.path.abspath(__file__)) #python file directory
@@ -16,7 +19,12 @@ for root, dirs, files in os.walk(mrcDir):
                 mrcPaths.append(os.path.join(root, file))
                 i += 1
 
+rotAll = []
+tiltAll = []
+psiAll = []
+
 for i in mrcPaths:
+    print(i)
     path = os.path.splitext(i)[0]
     state = os.path.split(path)[1] #'state_01_01', etc.
     alignOut = os.path.join(pyDir, 'STARs/proj812_%s.star' % state)
@@ -25,11 +33,19 @@ for i in mrcPaths:
 
     ##########################
     # import angles:
-    angles = pd.read_csv('proj812.txt', header=None, delim_whitespace=True) #projections
-    rot = angles[1]
-    tilt = angles[2]
-    psi = angles[3]
-
+    S2 = pd.read_csv('proj812.txt', header=None, delim_whitespace=True) #projections
+    if 1: #perform quaternion perturbations on each PD
+        rot, tilt = quatsGaussian.op(S2) 
+        psi = S2[3] #no in-plane rotations
+        
+        rotAll.append(rot)
+        tiltAll.append(tilt)
+        psiAll.append(psi)
+    else:
+        rot = S2[1]
+        tilt = S2[2]
+        psi = S2[3]
+    
     alignFile = open(alignOut, 'w')
     alignFile.write('\ndata_ \
             \n \
@@ -48,17 +64,24 @@ for i in mrcPaths:
             \n_rlnDefocusAngle #11 \
             \n_rlnCtfBfactor #12 \
             \n_rlnPhaseShift #13 \
+            \n_rlnPixelSize #14 \
             \n')
 
-    for i in range(0,len(rot)):  
-        df = np.random.randint(low=4000, high=12000, size=1)[0]
-        #df = 0
-        #x = np.random.uniform(-5,5)
-        #y = np.random.uniform(-5,5)
-        x = 0
-        y = 0
+    for i in range(0,len(rot)):
+        df = np.random.randint(low=2000, high=10000, size=1)[0]
+        if 1:
+            x = 0
+            y = 0
+        else: #x-, y-shifts on object's center in each image
+            x = np.random.uniform(-5,5)
+            y = np.random.uniform(-5,5)
 
-        alignFile.write('%.6f\t%.6f\t%.6f\t%.3f\t%.3f\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' \
-            % (rot[i],tilt[i],psi[i],x,y,float(df),float(df),300.,2.7,.1,0.,0.,0.))
+        alignFile.write('%.6f\t%.6f\t%.6f\t%.3f\t%.3f\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' \
+            % (rot[i],tilt[i],psi[i],x,y,float(df),float(df),300.,2.7,.1,0.,0.,0.,1.))
 
     alignFile.close()
+    
+
+
+    
+
