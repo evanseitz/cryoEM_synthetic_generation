3c2d24c7df0d964c6184865215d4773adf717ae8
Evan Seitz <35609503+evanseitz@users.noreply.github.com>
2019-12-02 12:53:23 -0500

    Update relion_projCTF.sh

--- 8_GenStacksCTF_relion/relion_projCTF.sh
+++ 8_GenStacksCTF_relion/relion_projCTF.sh
@@ -4,5 +4,5 @@ do
     angFile=/mnt/Data2/evanseitz/Hsp90_Online/7_GenSTARs_python/STARs/proj812_${out%.*}.star
     outFile=/mnt/Data2/evanseitz/Hsp90_Online/8_GenStacksCTF_relion/stacks/${out%.*}
     echo $outFile
-    relion_project --i "${i}" --o $outFile --ang $angFile --ctf true
+    relion_project --i "${i}" --o $outFile --ang $angFile --ctf true --angpix 1
 done
